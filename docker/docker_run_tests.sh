9fdfd9f9943925b2780e59656a6dda606459924d
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2022-09-11 10:01:43 +0200

    Add test solutions

---
+++ docker/docker_run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+DIR="$(cd -P "$(dirname "$0")" && pwd)"
+MOUNT_DIR="$(dirname "$DIR")"
+
+# The user inside the docker environment is `joyvan` with uid 1000
+# This setting is predefined in registry.git.rwth-aachen.de/jupyter/profiles/rwth-courses:latest
+# to ensure compatibility with RWTH Jupyter Hub
+# We will give this user id temporarly write permissions to this directory
+setfacl -R -m u:1000:rwx $MOUNT_DIR
+
+docker run \
+--name='ika-acdc-notebooks-tests' \
+--rm \
+--interactive \
+--tty \
+--publish 8888:8888 \
+--volume $MOUNT_DIR:/home/jovyan/acdc \
+tillbeemelmanns/acdc-notebooks:latest \
+/bin/bash -c \
+"papermill /home/jovyan/acdc/section_2_sensor_data_processing/1_semantic_image_segmentation_solution.ipynb /dev/null &&
+ papermill /home/jovyan/acdc/section_2_sensor_data_processing/2_augmentation_semantic_image_segmentation_solution.ipynb /dev/null"
+
+
+# Remove write permission of user 1000
+setfacl -R -x u:1000 $MOUNT_DIR
