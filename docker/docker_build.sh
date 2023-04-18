bffa9b8f901528da70e917f6db32297f7430fce6
till.beemelmanns@ika.rwth-aachen.de <depulsor@gmail.com>
2023-04-18 10:17:52 +0200

    update docker setup

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #docker buildx create --use
+
 docker buildx build \
---push \
 --platform linux/amd64,linux/arm64 \
 --cache-from type=registry,ref=rwthika/acdc-notebooks:latest \
 --tag rwthika/acdc-notebooks:latest \
+--output type=docker \
 .
