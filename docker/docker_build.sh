93f610a07ddb0926237c12d2659a71888d9e8074
Till Beemelmanns <till.beemelmanns@gmail.com>
2024-10-22 15:27:01 +0200

    delete .gitmodules

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -5,5 +5,5 @@ docker buildx build \
 --platform linux/amd64,linux/arm64 \
 --cache-from type=registry,ref=rwthika/acdc-notebooks:latest \
 --tag rwthika/acdc-notebooks:latest \
---output type=docker \
+--push \
 .
